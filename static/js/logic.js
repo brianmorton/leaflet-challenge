a6f0979d047a41961e8d91d5210f2969222cdbe0
Brian Morton <bmorton925@gmail.com>
2021-04-29 12:41:49 -0400

    legend partial

--- static/js/logic.js
+++ static/js/logic.js
@@ -24,11 +24,15 @@ d3.json(url).then(function(response) {
      
       // Set the data to variables
       var location = [response.features[i].geometry.coordinates[1],response.features[i].geometry.coordinates[0]];
+      location = Number(location)
       var depth = response.features[i].geometry.coordinates[2]
+      depth = Number(depth)
       var mag = response.features[i].properties.mag
       var place = response.features[i].properties.place
       var circleColor;
+
       
+
         if(depth > 250 ){
           circleColor = '#FF0000';
         }
@@ -39,19 +43,50 @@ d3.json(url).then(function(response) {
           circleColor = '#0000FF';
         }
         if(depth > 100 && mag < 150 ){
-          circleColor = '#0000FF';
+          circleColor = '#40E0D0';
         }
         if(depth > 0 && mag < 100 ){
           circleColor = '#FFFFFF';
         }
       }
-  
-L.circleMarker(location,{ 
+  //console.log(circleColor)
+var circles = L.circleMarker([location],{ 
   color: "black",
   fillColor: circleColor,
   fillOpacity: 1,
   radius:mag * 4,
   }).bindPopup("<h3>" + place + "</h3>").addTo(myMap);
 
-  //add legend
+  myMap.addLayer(circles)
+
+  //add legend inside d3 
+ 
+    var legend = L.control({ position: "bottomright" });
+    legend.onAdd = function() {
+      var div = L.DomUtil.create("div", "info legend");
+      
+      var limits = [250, 200,150, 100,0];
+      var colors = ['#FF0000','#00FF00','#0000FF','#40E0D0','#FFFFFF' ];
+      var labels = [];
+  
+      // Add min & max
+      var legendInfo = "<h1>Depth Legend</h1>" +
+        "<div class=\"labels\">" +
+          "<div class=\"min\">" + limits[0] + "</div>" +
+          "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+        "</div>";
+  
+      div.innerHTML = legendInfo;
+  
+      limits.forEach(function(limit, index) {
+        labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+      });
+  
+      div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+      return div;
+    };
+  
+    // Adding legend to the map
+    legend.addTo(myMap);
+
 })
